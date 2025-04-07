3f0c04d963357cd067a091a713f0f42e71e880a4
Andcool-Systems <drolll2007@gmail.com>
2025-04-07 15:06:06 +0300

    index now checking only affected pages

--- scripts/github.py
+++ scripts/github.py
@@ -15,3 +15,13 @@ def commit_history_file(repo_url: str, file_path: str):
         raise Exception(f'Cannot get commits list: {response.text}')
 
     return response.json()
+
+
+def get_commit_changes(repo_url: str, sha: str) -> dict:
+    response = requests.get(
+        f'{GITHUB_API_URL}/repos/{repo_url}/commits/{sha}', headers={'Authorization': f'Bearer {TOKEN}'})
+
+    if not response.ok:
+        raise Exception(f'Cannot get commit changes: {response.text}')
+
+    return response.json()
--- scripts/index.py
+++ scripts/index.py
@@ -1,13 +1,15 @@
-from github import commit_history_file
+import github
 import logging
 import os
 import re
 import json
+import sys
 from dotenv import load_dotenv
 load_dotenv()
 
 PAGES_PATH = 'pages'
 REPO_PATH = os.environ.get('REPO_PATH') or 'Andcool-Systems/blog-markdown'
+COMMIT_SHA = os.environ.get('COMMIT_SHA')
 RE_EXPR = r"[^\w\-\.~]"
 
 logger = logging.getLogger('Index')
@@ -50,19 +52,38 @@ def check_page(path: str) -> bool:
 with open('index.json', 'r', encoding='utf-8') as index_f:
     index_json = json.load(index_f)
 
+affected_files = github.get_commit_changes(REPO_PATH, COMMIT_SHA).get('files')
+if not affected_files:
+    logger.log(logging.ERROR, 'No affected files found, exiting...')
+    sys.exit(0)
 
-pages = os.listdir(PAGES_PATH)
-for page in pages:
+affected_paths = list({
+    '/'.join(file['filename'].split('/')[:2])
+    for file in affected_files
+    if file['filename'].startswith(f'{PAGES_PATH}/')
+})
+
+if not affected_paths:
+    logger.log(logging.ERROR, 'No affected pages found, exiting...')
+    sys.exit(0)
+
+for path in affected_paths:
+    page = path.replace(f'{PAGES_PATH}/', '')
     if re.search(RE_EXPR, page) or '/' in page:
         raise Exception(f'Page name contains forbidden characters!')
-
-    path = f'{PAGES_PATH}/{page}'
     check_page(path)
 
     logger.log(logging.INFO, 'Updating index...')
-    commits = commit_history_file(REPO_PATH, f'{path}/page.md')
-    collaborators = list(
-        set(map(lambda commit: commit['author']['login'], commits)))
+    commits = github.commit_history_file(REPO_PATH, f'{path}')
+
+    counts = {}
+    for commit in commits:
+        login = commit['author']['login']
+        counts[login] = counts.get(login, 0) + 1
+
+    collaborators = [login for login, _ in sorted(
+        counts.items(), key=lambda item: item[1], reverse=True)]
+
     file_meta = get_meta_for_page(path)
     if page not in index_json:
         author = commits[-1]['commit']['author']
